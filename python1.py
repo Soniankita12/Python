7694f46103783a47c8491ef92bff5398fcc9b955
soniankita12 <ankitasoni2211104@gmail.com>
2024-03-13 12:16:45 +0530

    Python day 1

---
+++ python1.py
@@ -0,0 +1,14 @@
+print("namste python")
+print ("Ankita","Soni")
+print(22)
+print(1+9)
+name =1
+age="a"
+print(1*age*(3+9))
+# here comment
+def sum(a,b):
+    res=a+b
+    return res
+
+print(sum(13,4))
+print(12//5)
